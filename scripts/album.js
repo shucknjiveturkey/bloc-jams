c5507b44e65c2b204cbce03f752a301110b0e32f
Mitch Barlow <barlowamitch@gmail.com>
2016-01-28 21:34:42 -0600

    play pause button

--- scripts/album.js
+++ scripts/album.js
@@ -193,7 +193,19 @@ var previousSong = function() {
     
 };
 
-
+var togglePlayFromPlayerBar = function(){
+	var $currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
+	if (currentSoundFile.isPaused()){
+		$currentlyPlayingCell.html(pauseButtonTemplate);
+		$(this).html(playerBarPauseButton);
+		currentSoundFile.play();
+	}
+	else if(currentSoundFile){
+		$currentlyPlayingCell.html(playButtonTemplate);
+		$(this).html(playerBarPlayButton);
+		currentSoundFile.pause();
+	}
+}
  var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 
  var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
@@ -207,10 +219,11 @@ var currentlyPlayingSongNumber = null;
  var currentVolume = 80;
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
+var $playPauseButton = $('.main-controls .play-pause');
 
  $(document).ready(function() {
      setCurrentAlbum(albumPicasso);
      $previousButton.click(previousSong);
      $nextButton.click(nextSong);
-     
+     $playPauseButton.click(togglePlayFromPlayerBar);
  });
